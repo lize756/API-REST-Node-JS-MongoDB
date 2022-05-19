018b8edac3368a38826a072acf43948cbd5df088
Fanny Lizeth Varela Castillo <lize756cas@gmail.com>
2022-05-19 01:34:16 -0500

    The add user was created

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoutes = require("./routes/user");
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(express.json());
+app.use("/api", userRoutes);
+
+//routes
+app.get("/", (req, res) => {
+  res.send("welcome to my API");
+});
+
+//mongoDB  connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected MongoDB Atlas"))
+  .catch((error) => console.error(error));
+
+app.listen(port, () => console.log("server listening on port", port));
---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    minlegth: 7,
+  },
+  identification: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
